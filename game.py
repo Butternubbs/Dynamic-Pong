0afd4270757be1997e367941f43584acc184da95
afoster <butternubbslardo@gmail.com>
2019-12-15 19:44:38 -0600

    Added background and basic pausing feature

--- game.py
+++ game.py
@@ -11,6 +11,7 @@ UNIQUE_KEYS = [(pygame.K_BACKQUOTE, pygame.K_1),
                (pygame.K_LEFTBRACKET, pygame.K_RIGHTBRACKET),
                (pygame.K_v, pygame.K_b), (pygame.K_k, pygame.K_l)]
 
+#Single player controls with left & right arrow keys
 #Player keys: (Supports up to 8 players)
 #1- ~ & 1
 #2- left & right arrow keys
@@ -69,13 +70,13 @@ class Paddle(pygame.sprite.Sprite):
     def update(self):
         pressed = pygame.key.get_pressed()
         if not MULTI:
-            if pressed[pygame.K_DOWN]:
+            if pressed[pygame.K_LEFT]:
                 dy = self.speed*math.sin(self.angle)
                 dx = -self.speed*math.cos(self.angle)
                 self.position[0] += dx
                 self.position[1] += dy
                 self.rect.center = self.position
-            elif pressed[pygame.K_UP]:
+            elif pressed[pygame.K_RIGHT]:
                 dy = -self.speed*math.sin(self.angle)
                 dx = self.speed*math.cos(self.angle)
                 self.position[0] += dx
@@ -102,6 +103,13 @@ pygame.display.set_caption("Pong")
 timer = pygame.time.Clock()
 
 def main():
+    background = pygame.Surface((600,600))
+    pygame.draw.circle(background, (200,50,0), (300,300), 440)
+    pygame.draw.circle(background, (160,40,0), (300,300), 400)
+    pygame.draw.circle(background, (120,30,0), (300,300), 360)
+    pygame.draw.circle(background, (80,20,0), (300,300), 320)
+    pygame.draw.circle(background, (40,10,0), (300,300), 280)
+    pygame.draw.circle(background, (0,0,0), (300,300), 240)
     ball_timer = 0
     rallies = 0
     for i in range(NUM_PADDLES):
@@ -110,29 +118,36 @@ def main():
         else:
             Paddle([300+250*math.sin(math.pi*i/(NUM_PADDLES/2)), 300+250*math.cos(math.pi*i/(NUM_PADDLES/2))], 100, 10, i*math.pi/(NUM_PADDLES/2), (255 - i*int(255/NUM_PADDLES),0, i*int(255/NUM_PADDLES)), UNIQUE_KEYS[0], paddles)
     ball = Ball((300,300), random.random() * math.pi*2, (5, 5))
+    paused = False
     while True:
-        screen.fill((0,0,0))
-        #pygame.draw.circle(screen, (255,255,255), (300,300), 250)
-        #pygame.draw.circle(screen, (0,0,0), (300,300), 245)
-        paddles.draw(screen)
-        for p in paddles.sprites():
-            p.update()
-        screen.blit(ball.image,ball.rect)
-        pygame.display.flip()
-        if ball_timer > 0:
-            ball_timer -= 1
-        else:
-            rallies += ball.update()
-        if ball.rect.left > 650 or ball.rect.right < -50 or ball.rect.top > 650 or ball.rect.bottom < -50:
-            ball = Ball((300,300), random.random() * math.pi*2, (5, 5))
-            ball_timer += 15
-            print("Rallies: " + str(rallies))
-            rallies = 0
+        if not paused:
+            screen.fill((0,0,0))
+            screen.blit(background, background.get_rect())
+            paddles.draw(screen)
+            for p in paddles.sprites():
+                p.update()
+            screen.blit(ball.image,ball.rect)
+            pygame.display.flip()
+            if ball_timer > 0:
+                ball_timer -= 1
+            else:
+                rallies += ball.update()
+            if ball.rect.left > 650 or ball.rect.right < -50 or ball.rect.top > 650 or ball.rect.bottom < -50:
+                ball = Ball((300,300), random.random() * math.pi*2, (5, 5))
+                ball_timer += 15
+                print("Rallies: " + str(rallies))
+                rallies = 0
         timer.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == pygame.K_SPACE:
+                    paused = not paused
 
 if __name__ == "__main__":
     main()
